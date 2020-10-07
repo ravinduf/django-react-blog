1a2ba8f474c701ef40ac84e3a008c547e2599ae0
ravinduf <ravinduf@rocketmail.com>
2020-10-07 14:48:58 +0530

    set view

--- backend/src/urls.py
+++ backend/src/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('summernote/', include('django_summernote.urls')),
+    path('', TemplateView.as_view(template_name='index.html'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
