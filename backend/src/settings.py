eb9d57834ce03c2b8322abcc62e241ab4fc30d3f
ravinduf <ravinduf@rocketmail.com>
2020-10-07 12:01:49 +0530

    added settings for rest-framework

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -136,3 +136,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/nedia/
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
+
+REST_FRAMEWORK = {
+    
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
