2e1270a024c1a1063268d64fa8d8e4fab395c460
ravinduf <ravinduf@rocketmail.com>
2020-10-08 13:14:30 +0530

    added summernote widget for blogpost model

--- backend/blog/admin.py
+++ backend/blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import BlogPost
+
+# Apply summernote to all TextField in model.
+class BlogPostAdmin(SummernoteModelAdmin):  # instead of ModelAdmin
+    summernote_fields = ('content',)
+
+admin.site.register(BlogPost, BlogPostAdmin)
 
 # Register your models here.
