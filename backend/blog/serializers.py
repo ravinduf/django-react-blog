1fc9c7ef5b0f7215f96160e30bdf5ea448d49c59
ravinduf <ravinduf@rocketmail.com>
2020-10-10 04:22:00 +0530

    fixed errors in blog/serializers.py

--- backend/blog/serializers.py
+++ backend/blog/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import BlogPost
 
-class BlogPostSerializer(serializers.ModelSerializers):
+class BlogPostSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = BlogPost
