555dd0f6e87390c8153d75251d5e9097e97a347c
ravinduf <ravinduf@rocketmail.com>
2020-10-10 04:23:08 +0530

    added blog.urls

--- backend/src/urls.py
+++ backend/src/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('summernote/', include('django_summernote.urls')),
+    path('blog', include('blog.urls')),
     path('', TemplateView.as_view(template_name='index.html'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
