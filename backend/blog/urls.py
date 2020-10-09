2c451cb372e7b0c0f770c62253e387ce280e634f
ravinduf <ravinduf@rocketmail.com>
2020-10-10 04:21:06 +0530

    fixed errors in urls

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -3,8 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('', BlogPostListView.as_view()),
-    path('/featured', BlogPostFeaturedView.as_view()),
-    path('/category', BlogPostCategoryView.as_view()),
-    path('/<slug>', BlogPostDetailView.as_view()),
+    path('featured', BlogPostFeaturedView.as_view()),
+    path('category', BlogPostCategoryView.as_view()),
+    path('<slug>', BlogPostDetailView.as_view()),
 ]
 
