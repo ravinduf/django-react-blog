8c98459cb7819fcbf5f327ad7e098f4482eca368
ravinduf <ravinduf@rocketmail.com>
2020-10-09 08:43:21 +0530

    created basic BlogPostDetailView class

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -13,3 +13,10 @@ class BlogPostListView(ListAPIView):
     serializer_class = BlogPostSerializer
     lookup_field = 'slug'
     permission_classes = (permissions.AllowAny, )
+
+class BlogPostDetailView(RetrieveAPIView):
+    queryset = BlogPost.objects.order_by('-date_created')
+    serializer_class = BlogPostSerializer
+    lookup_field = 'slug'
+    permission_classes = (permissions.AllowAny, )
+    
