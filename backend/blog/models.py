6944f6161510a592d7179f80631b5442a7359642
ravinduf <ravinduf@rocketmail.com>
2020-10-09 07:14:14 +0530

    fixed errors

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -21,7 +21,7 @@ class BlogPost(models.Model):
     title = models.CharField(max_length = 50)
     slug = models.SlugField()
     category = models.CharField(max_length = 50, choices = Categories.choices, default = Categories.WORLD)
-    thumbnail = models.ImageField(uploaded_to='photos')
+    thumbnail = models.ImageField(upload_to='photos')
     excerpt = models.CharField(max_length=150)
     month = models.CharField(max_length=3)
     day = models.CharField(max_length=2)
@@ -31,7 +31,7 @@ class BlogPost(models.Model):
     def save(self, *args, **kwargs):
         original_slug = slugify(self.title)
         slug = original_slug
-        queryset = BlogPost.objects.all().filter(slug__iexact=original_slug).count())
+        queryset = BlogPost.objects.all().filter(slug__iexact=original_slug).count()
 
         count = 1
         
