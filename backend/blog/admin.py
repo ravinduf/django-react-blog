2c2e2f4167ad22f5f774b53d452270371c524c3a
ravinduf <ravinduf@rocketmail.com>
2020-10-09 07:13:38 +0530

    customized admin panel by changing BlogPostAdmin

--- backend/blog/admin.py
+++ backend/blog/admin.py
@@ -4,6 +4,11 @@ from .models import BlogPost
 
 # Apply summernote to all TextField in model.
 class BlogPostAdmin(SummernoteModelAdmin):  # instead of ModelAdmin
+    exclude = ('slug',)
+    list_display = ('id', 'title', 'category', 'date_created')
+    list_display_links = ('id', 'title')
+    search_fields = ('title',)
+    list_per_page = 25
     summernote_fields = ('content',)
 
 admin.site.register(BlogPost, BlogPostAdmin)
