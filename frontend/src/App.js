beb759617b940609b87ad792ce0355eaada09487
ravinduf <ravinduf@rocketmail.com>
2020-10-11 11:46:09 +0530

    added code for the components to remove error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowerRouter as Router, Route, Switch} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 import Layout from './HOC/Layout'
 import Blog from "./components/Blog"
@@ -10,6 +10,7 @@ import Home from "./components/Home"
 
 function App() {
     return (
+       
         <Router>
             <Layout>
                 <Switch>
@@ -23,4 +24,4 @@ function App() {
     )
 }
 
-export default App
+export default App;
