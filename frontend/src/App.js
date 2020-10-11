e53a7c8977255a93c2ca83ccd8ba351453e6894b
ravinduf <ravinduf@rocketmail.com>
2020-10-11 11:11:24 +0530

    created basic components for the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,12 @@
 import React from 'react'
+import { BrowerRouter as Router, Route, Switch} from 'react-router-dom'
+
+import Layout from './HOC/Layout'
+import Blog from "./components/Blog"
+import BlogDetail from "./components/BlogDetail"
+import Category from "./components/Category"
+import Home from "./components/Home"
+
 
 function App() {
     return (
