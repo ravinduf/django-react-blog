78cdfe7230eb4a78ad4093ab56002cb521885c50
ravinduf <ravinduf@rocketmail.com>
2020-10-12 08:34:01 +0530

    changed the color of the navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,7 +4,7 @@ import { Nav, Navbar, } from 'react-bootstrap'
 const NavbarComponent = () => {
     return (
         <div>
-           <Navbar bg="dark" variant="dark" expand="lg">
+           <Navbar bg="primary" variant="dark" expand="lg">
                 <Navbar.Brand href="#home" style={NavBrandStyle} >Blog</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
