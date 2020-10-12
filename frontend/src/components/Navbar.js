4a6e75c93dd0a4468e893330d353b97594b36658
ravinduf <ravinduf@rocketmail.com>
2020-10-12 09:45:36 +0530

    fixed postioning

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -6,10 +6,10 @@ const NavbarComponent = () => {
     return (
         <div>
            <Navbar bg="primary" variant="dark" expand="lg">
-                <Navbar.Brand href="#home" style={NavBrandStyle} >Blog</Navbar.Brand>
+                <Navbar.Brand href="#home" style={NavBrandStyle} >Blog-World</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="mr-auto">
+                        <Nav className="mr-auto" >
                             <Link style={NavLinkStyle} to="/">Home</Link>
                             <Link style={NavLinkStyle} to="/blog">Blog</Link>
                    
@@ -21,7 +21,6 @@ const NavbarComponent = () => {
     )
 }
 
-
 const NavBrandStyle = {
     fontSize: '34px',
     marginLeft: '50px'
@@ -29,10 +28,12 @@ const NavBrandStyle = {
 
 const NavLinkStyle = {
     fontSize: '20px',
-    marginRight: '10px',
-    marginTop: '8px',
+    margin: ' 8px 20px 0px',
     color: 'white',
-    textDecoration: 'none'
+    textDecoration: 'none',
+    '&:hover': {
+        textDecoration: 'underline'
+    }
 }
 
 export default NavbarComponent
