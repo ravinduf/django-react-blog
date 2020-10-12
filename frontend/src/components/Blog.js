927a665a8138836616c7c12d67b9f2cd479aa20b
ravinduf <ravinduf@rocketmail.com>
2020-10-13 01:01:34 +0530

    added functions to get Featured blog and blogs from database

--- frontend/src/components/Blog.js
+++ frontend/src/components/Blog.js
@@ -9,15 +9,29 @@ const Blog = () => {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                
+                const res = await axios.get(`${process.env.REACT_APP_API_URL}/api/blog/featured`);
+                console.log(res);
+                setFeaturedBlog(res.data[0]);
             }
             catch(err) {
-                console.log(error);
+                console.log(err);
             }
         };
 
-        fetchdatat();
+        const fetchBlogs = async () => {
+            try{
+                const res = await axios.get(`${process.env.REACT_APP_API_URL}/api/blog/`);
+                console.log(res);
+                setBlogs(res.data);
+            }
+            catch(err) {
+                console.log(err);
+            }
 
+        }
+
+        fetchData();
+        fetchBlogs();
     }, []);
 
     return (
