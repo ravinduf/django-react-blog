82a199ac5ae6abaa808a256721bd482e4f43e65b
ravinduf <ravinduf@rocketmail.com>
2020-10-13 00:19:31 +0530

    created state blogs and featuredBlogs

--- frontend/src/components/Blog.js
+++ frontend/src/components/Blog.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Blog = () => {
+    const [blogs, setBlogs] = useState([]);
+    const [featuredBlog, setFeaturedBlog] = useState([]);
+    
     return (
         <div>
             <h1>Blog</h1>
