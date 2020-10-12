2107055da2065cbcd067ccc6bd520e5544098634
ravinduf <ravinduf@rocketmail.com>
2020-10-13 00:25:40 +0530

    updated

--- frontend/src/components/Blog.js
+++ frontend/src/components/Blog.js
@@ -1,9 +1,25 @@
 import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 const Blog = () => {
     const [blogs, setBlogs] = useState([]);
     const [featuredBlog, setFeaturedBlog] = useState([]);
-    
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                
+            }
+            catch(err) {
+                console.log(error);
+            }
+        };
+
+        fetchdatat();
+
+    }, []);
+
     return (
         <div>
             <h1>Blog</h1>
