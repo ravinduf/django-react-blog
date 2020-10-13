5b400d36df966f78dd841dfcaa47b7c60aefe1df
ravinduf <ravinduf@rocketmail.com>
2020-10-13 20:15:06 +0530

    created basic structure in BlogDetail.js

--- frontend/src/components/BlogDetail.js
+++ frontend/src/components/BlogDetail.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Container } from 'react-bootstrap'
 
 const BlogDetail = () => {
+
+    const [blog, setBlog] = useState({});
+    const createBlog = () => {
+        return {__html: blog.content }
+    };
+
     return (
-        <div>
-            
-        </div>
+        <Container className="mt-3">
+            <h1 className="display-2"></h1>
+            <h2 className="text-muted mt-3">Category: category</h2>
+            <h4>month day</h4>
+
+            <div className="mt-5 mb-5" dangerouslySetInnerHTML={createBlog()} />
+        </Container>
     )
 }
 
