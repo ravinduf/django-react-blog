644f5d4d8a32ba54135058102f0c5a0e70f27fc3
ravinduf <ravinduf@rocketmail.com>
2020-10-13 22:03:12 +0530

    added category nav to Category.js

--- frontend/src/components/Category.js
+++ frontend/src/components/Category.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import { Container, Nav } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+
 
 const Category = () => {
     return (
-        <div>
-            <h1>Category</h1>
-        </div>
+        <Container className="mt-3">
+            <div className="py-1 mb-2">
+                <Nav className="d-flex justify-content-between">
+                    <Link className="p-2 text=muted" to="/category/world">World</Link>
+                    <Link className="p-2 text=muted" to="/category/technology">Technology</Link>
+                    <Link className="p-2 text=muted" to="/category/environment">Environment</Link>
+                    <Link className="p-2 text=muted" to="/category/design">Design</Link>
+                    <Link className="p-2 text=muted" to="/category/culture">Culture</Link>
+                    <Link className="p-2 text=muted" to="/category/business">Business</Link>
+                    <Link className="p-2 text=muted" to="/category/politics">Politics</Link>
+                    <Link className="p-2 text=muted" to="/category/Opinion">Opinion</Link>
+                    <Link className="p-2 text=muted" to="/category/Science">Science</Link>
+                    <Link className="p-2 text=muted" to="/category/Health">Health</Link>
+                    <Link className="p-2 text=muted" to="/category/Style">Style</Link>
+                    <Link className="p-2 text=muted" to="/category/Travel">Travel</Link>
+
+                </Nav>
+            </div>
+        </Container>
     )
 }
 
