7cdb3feb308d712013fc4c963738064f075dcce2
ravinduf <ravinduf@rocketmail.com>
2020-10-13 07:42:55 +0530

    created nav for categories at Blog

--- frontend/src/components/Blog.js
+++ frontend/src/components/Blog.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
+import { Container, Nav } from 'react-bootstrap'
 
 const Blog = () => {
     const [blogs, setBlogs] = useState([]);
@@ -34,10 +35,33 @@ const Blog = () => {
         fetchBlogs();
     }, []);
 
+    const capitalizeFirstLetter = (word) => {
+        if (word)
+            return word.charAt(0).toUpperCase() + word.slice(1);
+        
+        return '';
+    }
+
     return (
-        <div>
-            <h1>Blog</h1>
-        </div>
+        <Container className="mt-3">
+            <div className=" py-1 mb-2">
+                <Nav className="d-flex justify-content-between">
+                    <Link className="p-2 text=muted" to="/category/world">World</Link>
+                    <Link className="p-2 text=muted" to="/category/technology">Technology</Link>
+                    <Link className="p-2 text=muted" to="/category/environment">Environment</Link>
+                    <Link className="p-2 text=muted" to="/category/design">Design</Link>
+                    <Link className="p-2 text=muted" to="/category/culture">Culture</Link>
+                    <Link className="p-2 text=muted" to="/category/business">Business</Link>
+                    <Link className="p-2 text=muted" to="/category/politics">Politics</Link>
+                    <Link className="p-2 text=muted" to="/category/Opinion">Opinion</Link>
+                    <Link className="p-2 text=muted" to="/category/Science">Science</Link>
+                    <Link className="p-2 text=muted" to="/category/Health">Health</Link>
+                    <Link className="p-2 text=muted" to="/category/Style">Style</Link>
+                    <Link className="p-2 text=muted" to="/category/Travel">Travel</Link>
+
+                </Nav>
+            </div>
+        </Container>
     )
 }
 
